9d28288d5591303f0ee10318100542d9d30de6e6
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-21 06:46:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,9 @@ def self.artists
   end
   
 def self.genre_count
-     @@genre_counts.for do 
+     @@genre_counts.for do |genre, count|
+     @@genre_counts[genre] ||= 0
+     @@genre_counts[genre] +=1
+     end
 end
 end
