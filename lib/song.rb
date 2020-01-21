1afccdfc60d3c8850e3a4c6c18df0079ef8fa5b0
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-21 06:42:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,6 +15,8 @@ def initialize(name, artist, genre)
   @@genres << genre
   @@artists << artist
   @@genre_counts[@genre]||=0
+  @@genre_counts[@genre].each do |genre|
+   @@genre_counts[@genre] += 1
   end
 
 def self.count
@@ -30,8 +32,7 @@ def self.artists
   end
   
 def self.genre_count
-  @@genre_counts[@genre].each do |genre|
-   @@genre_counts[@genre] += 1
+  
    @@genre_counts
   end
 end
